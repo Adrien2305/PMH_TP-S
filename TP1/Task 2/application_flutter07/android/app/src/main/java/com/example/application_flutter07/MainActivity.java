24c20fb6859d23e7c30e266f14deb6617e31de37
Adrien <alesther@student.udm.ac.mu>
2023-05-06 11:57:47 +0400

    TP1

---
+++ TP1/Task 2/application_flutter07/android/app/src/main/java/com/example/application_flutter07/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.application_flutter07;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ TP1/Task 3/application_flutter_tp1_task3/android/app/src/main/java/com/example/application_flutter_tp1_task3/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.application_flutter_tp1_task3;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
